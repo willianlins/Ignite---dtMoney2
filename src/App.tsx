efbc9651608ef1c1583eeec39d9949251d764574
Willian Lins Araújo <willian.araujo@anchieta.br>
2022-04-06 14:06:00 -0300

    finalizado duas vezes

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactioModal';
+
+import {TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionMondal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTrasactionModal={handleOpenNewTransactionMondal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
